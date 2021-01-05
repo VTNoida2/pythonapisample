2b1b45bd91eb4feb8a0924c35833a53c13badd31
Rajan Shrivastava <rajan@vidyatech.com>
2021-01-05 13:17:52 +0530

    functionedit

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():    
     return render_template("index.html")
 
+@app.route("/")
+def hello():
+    return "Hello, World!"	
+	
 if __name__ == '__main__':
     app.run(debug=True)
         
